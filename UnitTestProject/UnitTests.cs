0d7ce58b22f3e3877e9b38d6c9c6f5996bc5bbdc
Bryan Kraitberg <Bryan.Kraitberg@varian.com>
2016-03-02 13:03:02 -0600

    Add new test for exercise

--- UnitTestProject/UnitTests.cs
+++ UnitTestProject/UnitTests.cs
@@ -207,6 +207,26 @@
             }
         }
 
+        //[Test]
+        //public void Test_UserCanExitByEnteringQuit()
+        //{
+        //    using (var writer = new StringWriter())
+        //    {
+        //        Console.SetOut(writer);
+
+        //        using (var reader = new StringReader("Jason\r\nsfa\r\nquit\r\n\r\n"))
+        //        {
+        //            Console.SetIn(reader);
+
+        //            Tusc tusc = new Tusc(users, products);
+        //            tusc.Start();
+        //        }
+
+        //        Assert.IsTrue(writer.ToString().Contains("Type quit to exit the application"));
+        //        Assert.IsTrue(writer.ToString().Contains("Press Enter key to exit"));
+        //    }
+        //}
+
         private static T DeepCopy<T>(T obj)
         {
             using (MemoryStream stream = new MemoryStream())
